11c2cb095014ffc7db5be4d3e16e5f7572561208
Productive Programmer <charles.lobo@gmail.com>
2018-09-28 13:53:00 +1000

    Improve testing code

--- test.js
+++ test.js
@@ -12,22 +12,66 @@ const client = new cote.Requester({
  * TODO: Add test cases for all endpoints
  */
 function main() {
-    client.send({ type: 'new-msg', msg: { type: "test" } }, (err) => {
-        if(err) console.error(err)
-        else {
-            console.log('posted message');
-            client.send({
-                type: 'dump-msgs',
-                opts: {
-                    showPvt: true,
-                    showCnt: false,
-                    showAth: false,
-                },
-            }, (err, msgs) => {
-                if(err) u.showErr(err)
-                else u.showMsg(msgs)
-            })
-        }
+    let args = process.argv
+    if(args.length == 2) showHelp()
+    let cmd = args[2]
+    if(cmd == 'dump') dump(args[3])
+    if(cmd == 'msg') add(args[3])
+    if(cmd == 'invite') invite(args[3])
+    if(cmd == 'join') join(args[3])
+}
+
+function showHelp() {
+    u.showMsg(`
+Help:
+    dump <id>:   show feed <for user>
+    msg txt: add message with text
+    join invite: use the invite to join a pub
+`)
+}
+
+function dump(user) {
+    let req = {
+        type: 'dump-msgs',
+        opts: {
+            showPvt: true,
+            showCnt: false,
+            showAth: false,
+        },
+    }
+    if(user) req.user = user
+    client.send(req, (err, msgs) => {
+        if(err) u.showErr(err)
+        else u.showMsg(msgs)
+
+        process.exit()
+    })
+}
+
+function add(txt) {
+    client.send({ type: 'new-msg', msg: { type: "test", txt:txt } }, (err) => {
+        if(err) u.showErr(err)
+        else u.showMsg('Ok')
+
+        process.exit()
+    })
+}
+
+function invite() {
+    client.send({ type: 'create-invite' }, (err, inv) => {
+        if(err) u.showErr(err)
+        else u.showMsg(inv)
+
+        process.exit()
+    })
+}
+
+function join(inv) {
+    client.send({ type: 'accept-invite', invite:inv }, (err) => {
+        if(err) u.showErr(err)
+        else u.showMsg('Ok')
+
+        process.exit()
     })
 }
 
